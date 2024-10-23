e63fa908e174e09f20b35c182cb465a383ec3669
Tiago Ferreira <tiago.dev@agroflux.ind.br>
2024-10-23 10:31:28 -0300

    feat: add the config to redirection

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -7,16 +7,28 @@ export const authConfig = {
     signIn: '/login',
   },
   callbacks: {
+    // The 'authorized' callback should only return true or false to allow or deny access
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+
       if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; 
-      } else if (isLoggedIn) {
-        return NextResponse.redirect(new URL('https://next-js-training-prod.vercel.app/dashboard', nextUrl));
+        return isLoggedIn ? true : false; // Allow access if logged in, otherwise deny
       }
+
+      // For other pages, return true (allow access)
       return true;
+    },
+    
+    // Use the 'redirect' callback to handle redirection after login
+    async redirect({ url, baseUrl }) {
+      const isDashboard = url.includes('/dashboard');
+      if (isDashboard) {
+        // Redirect to dashboard if it's part of the URL
+        return `${baseUrl}/dashboard`;
+      }
+      // Default behavior: redirect to the base URL or continue with the original URL
+      return url.startsWith(baseUrl) ? url : baseUrl;
     },
   },
   providers: [],
